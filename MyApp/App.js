4941f22d92c08fecf57dba3a2cb42c61a010f2e6
Clement Le Bas <clement.lebas77@live.fr>
2020-04-03 11:16:24 +0200

    [ADDED] list of Rick and Morty character fetch with api

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,6 +1,14 @@
-import React, {useState} from 'react';
-import {StyleSheet, ScrollView, View, Text, StatusBar} from 'react-native';
+import React, {useState, Component} from 'react';
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  FlatList,
+} from 'react-native';
 import {ListItem} from 'react-native-elements';
+import axios from 'axios';
 
 import {
   Header,
@@ -10,30 +18,64 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content">
-        <Text>StatusBar</Text>
-      </StatusBar>
-      <>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <View style={styles.body}>
-            <Text>Body</Text>
-            <View style={styles.sectionContainer}>
-              <Text>Container 1</Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text>Container 2</Text>
-            </View>
-          </View>
-        </ScrollView>
-      </>
-    </>
+class App extends Component {
+  componentDidMount() {
+    axios.get('https://rickandmortyapi.com/api/character').then(response => {
+      console.log('response', response);
+
+      this.setState({list: response.data.results});
+    });
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+    };
+  }
+
+  keyExtractor = (item, index) => index.toString();
+
+  renderItem = ({item}) => (
+    <ListItem
+      title={item.name}
+      subtitle={'Status: ' + item.status}
+      leftAvatar={{source: {uri: item.image}}}
+      bottomDivider
+    />
   );
-};
+
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="dark-content">
+          <Text>StatusBar</Text>
+        </StatusBar>
+        <>
+          <View style={styles.body}>
+            <FlatList
+              keyExtractor={this.keyExtractor}
+              data={this.state.list}
+              renderItem={this.renderItem}
+            />
+          </View>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            <View style={styles.body}>
+              {/* <View style={styles.sectionContainer}>
+                <Text>Container 1</Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text>Container 2</Text>
+              </View> */}
+            </View>
+          </ScrollView>
+        </>
+      </>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   scrollView: {
